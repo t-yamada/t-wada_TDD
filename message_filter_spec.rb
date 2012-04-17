f24f3468dc18902df1cf086f7ff7cca9d30a1f0b
Taku Yamada <t-yamada@groovenauts.jp>
2012-04-17 21:12:31 +0900

    itの改行をなくして見やすくした

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -4,10 +4,6 @@ require_relative 'message_filter'
 
 describe MessageFilter ,'with argument "foo"' do
   subject { MessageFilter.new('foo') }
-  it {
-    should be_detect('hello from foo')
-  }
-  it {
-    should_not be_detect('hello,world!')
-  }
+  it { should be_detect('hello from foo') }
+  it { should_not be_detect('hello,world!') }
 end
