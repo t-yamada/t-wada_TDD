2c3e01d27822d5d6569001cb9f29541ade2498da
Taku Yamada <t-yamada@groovenauts.jp>
2012-04-17 21:01:32 +0900

    add another test code

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -7,4 +7,8 @@ describe MessageFilter do
     filter = MessageFilter.new('foo')
     filter.detect?('hello from foo').should == true
   end
+  it 'should detect message without NG word' do
+    filter = MessageFilter.new('foo')
+    filter.detect?('hello,world!').should == false
+  end
 end
